51cfd96895d4b8cd1d1a30511d7465a672ee9032
martier <martier@oocl.com>
2021-08-19 00:48:04 +0800

    [ReactTodoList][Eric] feat: add Slider

--- src/App.js
+++ src/App.js
@@ -2,37 +2,49 @@ import './App.css';
 // import TodoForm from './features/components/TodoForm';
 // import TodoGroup from './features/components/TodoGroup';
 import TodoList from './features/components/TodoList';
-import { Route, Link, BrowserRouter, Switch } from "react-router-dom";
-import React from 'react';
+
 import NotFoundPage from './features/notfound/NotFoundPage';
 import DoneList from './features/components/DoneList';
+import { Route, Link, BrowserRouter, Switch } from "react-router-dom";
+import React from 'react';
+import { Tabs } from 'antd';
+import 'antd/dist/antd.css';
 
+
+const { TabPane } = Tabs.TabPane;
 function App() {
   return (
     <React.Fragment>
-
       <BrowserRouter>
+          <div className="tabcontent"><h2><Link to="/">To Do List</Link></h2></div>
+          <div className="tabcontent"><h2><Link to="/done">Done List</Link></h2></div>
+          <div className="container">
 
-        <div className="tabcontent"><h2><Link to="/">To Do List</Link></h2></div>
-        <div className="tabcontent"><h2><Link to="/done">Done List</Link></h2></div>
-        <div className="container">
-
-          <header className="App">
-            <p>Hello React!</p>
-          </header>
-          <Switch>
-            <Route exact path="/" component={TodoList}></Route>
-            <Route exact path="/done" component={DoneList}></Route>
-            <Route exact path="*" component={NotFoundPage}></Route>
-          </Switch>
-        </div>
+            <header className="App">
+              <p>Hello React!</p>
+            </header>
+            <Switch>
+              <Route exact path="/" component={TodoList}></Route>
+              <Route exact path="/done" component={DoneList}></Route>
+              <Route exact path="*" component={NotFoundPage}></Route>
+            </Switch>
+          </div>
       </BrowserRouter>
     </React.Fragment>
-    // <div className="App">
-    // <h1>Hello, TodoList</h1>
-    //   <TodoGroup/>
-    //   <TodoForm/>
-    // </div>
+
+
+          /* <div>
+            <BrowserRouter>
+              <div className="tabcontent"><h2><Link to="/">To Do List</Link></h2></div>
+              <div className="tabcontent"><h2><Link to="/done">Done List</Link></h2></div>
+              <Switch>
+                <Route exact path="/" component={TodoList}></Route>
+                <Route exact path="/done" component={DoneList}></Route>
+                <Route exact path="*" component={NotFoundPage}></Route>
+              </Switch>
+            </BrowserRouter>
+          </div> */
+          
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,108 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+// import { Provider } from 'react-redux';
+// import store from "./store/store";
+// import 'antd/dist/antd.css';
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+import App from './App';
+import { Provider } from 'react-redux';
+import store from "./store/store";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from "./store/store";
-import 'antd/dist/antd.css';
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UnorderedListOutlined,
+  FileDoneOutlined,
+  NumberOutlined
+} from '@ant-design/icons';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+// import { Route, Link, BrowserRouter, Switch } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import TodoList from './features/components/TodoList';
+// import NotFoundPage from './features/notfound/NotFoundPage';
+import DoneList from './features/components/DoneList';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const { Header, Sider, Content } = Layout;
+
+class Index extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <Router>
+      <Layout >
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+          <div className="logo" >To Do List</div>
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']} >
+            <Menu.Item key="1" icon={<UnorderedListOutlined />} className="menuItem">
+              Manage Lists<Link to="/" />
+            </Menu.Item>
+            <Menu.Item key="2" icon={<FileDoneOutlined />} className="menuItem">
+              View Done Lists<Link to="/done" />
+            </Menu.Item>
+            <Menu.Item key="3" icon={<NumberOutlined />} className="menuItem">
+              Version<Link to="/ds" />
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout" style={{ height: '1000px' }}>
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: this.toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              maxHeight: '100%'
+            }}
+          >
+            <Provider store={store}>
+              <Switch>
+              <Route exact path="/" component={TodoList}></Route>
+              <Route exact path="/done" component={DoneList}></Route>
+            </Switch>
+            </Provider>
+          </Content>
+        </Layout>
+      </Layout>
+      </Router>
+    );
+  }
+}
+
+ReactDOM.render(<Index />, document.getElementById('root'));
+
+export default Index;
